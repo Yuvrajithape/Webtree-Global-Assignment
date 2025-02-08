af4e961493f22a1de6cbae7061195a2acb928a35
Yuvraj_Ithape <yuvrajithape9696@gmail.com>
2025-02-08 11:08:57 +0530

    feat: implemented profile card

--- User_Profile/src/Components/ProfileCard.jsx
+++ User_Profile/src/Components/ProfileCard.jsx
@@ -4,19 +4,18 @@ const ProfileCard = ({ user }) => {
     if (!user) return <p>Loading...</p>;
 
     return (
-        <div className="main border-1 border-blue h-60 p-5 flex items-center space-x-4 bg-white shadow-lg rounded-lg">
-            {/* Profile Image */}
-            <div className="w-35 h-35 border-1 border-black">
+        <div className="main border-1 border-blue h-50 p-5 flex items-center space-x-4 bg-gradient-to-r from-yellow-50 to-yellow-100 shadow-lg rounded-lg">
+            <div className="w-30 h-30 border-1 border-black hover:scale-110 hover:shadow-xl transition-transform duration-300">
                 <img src={user.picture.large} alt="Profile" className="w-full h-full object-cover rounded-md" />
             </div>
-            <div className="flex flex-col justify-center">
-                <div className="flex space-x-5 items-center ml-5 mb-10">
-                    <span className="font-semibold ">{user.name.first}</span>
-                    <span className="font-semibold">{user.name.last}</span>
-                </div>
 
-                <p className="mt-1">Gender: {user.gender}</p>
-                <p className="mt-1">Phone: {user.phone}</p>
+            <div className="flex flex-col justify-center">
+                <div className="flex space-x-2 items-center ml-3 mb-3">
+                    <span className="font-semibold text-[20px]">{user.name.first}</span>
+                    <span className="font-semibold text-[20px]">{user.name.last}</span>
+                </div>
+                <p className="ml-3 text-[18px] mb-2 ">Gender: {user.gender}</p>
+                <p className="ml-3 text-[18px] ">Phone: {user.phone}</p>
             </div>
         </div>
     );
